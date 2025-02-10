c566d60cd0a9ebc9ff6e8f061642a2c40d4aeabe
oadebayo4 <oadebayo4@ivytech.edu>
2025-02-09 21:26:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// setup.. this is similar to when we use our default tags in html
+
+const express = require("express")
+var cors = require("cors")
+
+// activate or tell this app variable to be an express server
+const app = express()
+app.use(cors())
+const router = express.Router()
+
+// making an api using routes
+// Routes are used to handle browser request. The look like URLs. The difference is that when a browser requests a route, it is dynamically handled by using a function.
+
+router.get("/songs", function(req,res){
+    const songs = [
+    {
+        title: "We Found Love",
+        artist: "Rihanna",
+        popularity: 10,
+        releaseDate: new Date(2011, 9, 22),
+        genre: ["electro house"]
+    },
+    {
+        title: "Happy",
+        artist: "Pharrell William",
+        popularity: 10,
+        releaseDate: new Date (2013, 11, 21),
+        genre: ["soul", "new soul"]
+    }
+
+    
+];
+res.json(songs)
+})
+
+//all requests that usually use an api start with /api... so the url would be localhost:3000/api/songs
+app.use("/api", router)
+app.listen(3000)
